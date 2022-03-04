e22a821d9f0dd28a56b4a07cb10d21160574e740
Songmi <ksongmi0727@gmail.com>
2022-03-04 18:31:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/algorithm/Ex.kt
@@ -0,0 +1,25 @@
+package com.example.algorithm
+
+
+//10171
+
+//fun main(args:Array<String>){
+//    println("\\    /\\\n" +
+//            " )  ( ')\n" +
+//            "(  /  )\n" +
+//            " \\(__)|")
+//}
+
+
+//10172
+
+//fun main(args:Array<String>){
+//    println("|\\_/|\n" +
+//            "|q p|   /}\n" +
+//            "( 0 )\"\"\"\\\n" +
+//            "|\"^\"`    |\n" +
+//            "||_/=\\\\__|"
+//    )
+//}
+
+
