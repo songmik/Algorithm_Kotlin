a56ae725cc78c7fff4a153a34d3afdde54d82d5f
Songmi <ksongmi0727@gmail.com>
2022-03-09 21:32:48 +0900

    0309

---
+++ app/src/main/java/com/example/algorithm/Ex_2.kt
@@ -0,0 +1,44 @@
+package com.example.algorithm
+
+
+//1330번
+
+//import java.util.*
+//
+//fun main() = with(Scanner(System.`in`)){
+//    val a = nextInt()
+//    val b = nextInt()
+//
+//    if (a > b){
+//        println(">")
+//    } else if (a < b) {
+//        println("<")
+//    } else {
+//        println("==")
+//    }
+//}
+
+
+//9498번
+
+//import java.util.*
+//
+//fun main() = with(Scanner(System.`in`)){
+//    val score = nextInt()
+//
+//    if (score >= 90){
+//        println("A")
+//    } else if (score >= 80) {
+//        println("B")
+//    } else if (score >= 70) {
+//        println("C")
+//    } else if (score >= 60) {
+//        println("D")
+//    } else {
+//        println("F")
+//    }
+//}
+
+
+//2753번
+
