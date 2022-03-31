82e293e90f28ffe426475cd56e3175479a5f7521
Songmi <ksongmi0727@gmail.com>
2022-03-31 21:13:42 +0900

    0331

---
+++ app/src/main/java/com/example/algorithm/LeetCode.kt
@@ -0,0 +1,50 @@
+package com.example.algorithm
+
+
+// 1. TwoSum
+
+
+//방법 1
+class Solution {
+    fun twoSum(nums: IntArray, target: Int) : IntArray {
+        val map = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, int ->
+            map[int]?.let { return intArrayOf(it, index) }
+            map[target - int] = index
+        }
+        return intArrayOf()
+    }
+}
+
+//방법 2
+class SolutionTwo {
+    fun twoSum(nums: IntArray, target: Int) : IntArray {
+        val map = mutableMapOf<Int, Int>()
+        nums.forEachIndexed{ index, num ->
+            val com = target - num
+            val other = map.get(com)
+            when {
+                other != null -> return intArrayOf(other, index)
+                else -> map[num] = index
+            }
+        }
+        return IntArray(2)
+    }
+}
+
+// 방법 3
+class SolutionThree{
+    fun twoSum(nums: IntArray, target: Int) :IntArray{
+        val map : MutableMap<Int, Int> = mutableMapOf()
+        nums.forEachIndexed { index, number ->
+            val compliement = target - number
+            if (map.containsKey(compliement)) return intArrayOf(map[compliement]!!, index)
+            map[number] = index
+        }
+        throw IllegalArgumentException("No two sum solution")
+    }
+}
+
+
+
+// 2.
