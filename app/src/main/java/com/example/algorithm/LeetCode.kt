d9846363273b5918277e522f1f54bc9045257cf7
Songmi <ksongmi0727@gmail.com>
2022-04-01 22:31:28 +0900

    0401

--- app/src/main/java/com/example/algorithm/LeetCode.kt
+++ app/src/main/java/com/example/algorithm/LeetCode.kt
@@ -46,5 +46,20 @@ class SolutionThree{
 }
 
 
+//방법 4
+class SolutionFore{
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = hashMapOf<Int, Int>()
+        nums.forEachIndexed { index, i ->
+            val goal = target -i
+            if (map[goal] != null) return intArrayOf(map[goal]!!, index)
+            map[i] = index
+        }
+        return intArrayOf(-1, -1)
+    }
+}
+
+
+
 
 // 2.
