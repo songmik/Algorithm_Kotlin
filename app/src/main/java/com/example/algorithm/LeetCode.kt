d785def4cebee2070d2d4f22a002e44fece493ff
Songmi <ksongmi0727@gmail.com>
2022-04-18 23:48:50 +0900

    0418

--- app/src/main/java/com/example/algorithm/LeetCode.kt
+++ app/src/main/java/com/example/algorithm/LeetCode.kt
@@ -297,3 +297,20 @@ class Solution_number69 {
         return right
     }
 }
+
+
+// 70. Climbing Stairs
+class Solution_number70 {
+    fun climbStairs(n: Int): Int {
+        val dp = Array<Int>(n +1) { 0 }
+        dp.forEachIndexed() {index, _ ->
+            when(index){
+                0 -> dp[index] = 0
+                1 -> dp[index] = 1
+                2 -> dp[index] = 2
+                else -> dp[index] = dp[index -1] + dp[index -2]
+            }
+        }
+        return dp[n]
+    }
+}
